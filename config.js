3385248cec3b9dd81799bf10bc5d5e21fbe50d17
fentonc2013 <fentonc2013@users.noreply.github.com>
2026-04-27 04:06:12 +0000

    deploy: acfa882179138642441921809c7b00967c982e35

---
+++ config.js
@@ -0,0 +1,10 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD93i7nzIQK_Zg2JsbORfjBvnRAqzj25SM",
+  authDomain: "pawcals.firebaseapp.com",
+  projectId: "pawcals",
+  storageBucket: "pawcals.firebasestorage.app",
+  messagingSenderId: "456619464502",
+  appId: "1:456619464502:web:d9603311111b6debfe0597"
+};
+;
+const APP_SALT = 'pawcals-v1-2026';
